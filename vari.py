3e4cb69661f6f40533c49c007a25c7b297f7942c
navaneeth <navaneeth@ideapad.mshome.net>
2026-05-16 19:04:19 +0530

    third commit mate

---
+++ vari.py
@@ -0,0 +1,6 @@
+age = input("enter the age please")
+age = int(age)
+name = "Bholo kumar"
+gpa = 7.45
+blah={"stAge":age,"stName":name,"stGpa":gpa}
+print("the student",blah["stName"],"of age",blah["stAge"],"has gpa",blah["stGpa"])
